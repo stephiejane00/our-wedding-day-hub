880d0042793921923e71764be93283e377d78d4f
stephiejane00 <stephiejane00@gmail.com>
2026-03-12 13:52:35 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function filterVendors(category){
-    let vendors = document.querySelectorAll(".vendor-card");
-    vendors.forEach(vendor=>{
-        if(category==="all"){
-            vendor.style.display="block";
-        } else if(vendor.classList.contains(category)){
-            vendor.style.display="block";
-        } else {
-            vendor.style.display="none";
-        }
-    });
+let vendors = document.querySelectorAll(".vendor-card");
+vendors.forEach(vendor=>{
+    if(category==="all"){
+        vendor.style.display="block";
+    } else if(vendor.classList.contains(category)){
+        vendor.style.display="block";
+    } else {
+        vendor.style.display="none";
+    }
+});
 }
 
 function searchVendors(){
-    let input = document.getElementById("search").value.toLowerCase();
-    let vendors = document.querySelectorAll(".vendor-card");
-    vendors.forEach(vendor=>{
-        let text = vendor.innerText.toLowerCase();
-        vendor.style.display = text.includes(input) ? "block" : "none";
-    });
+let input = document.getElementById("search").value.toLowerCase();
+let vendors = document.querySelectorAll(".vendor-card");
+vendors.forEach(vendor=>{
+    let text = vendor.innerText.toLowerCase();
+    vendor.style.display = text.includes(input) ? "block" : "none";
+});
 }
