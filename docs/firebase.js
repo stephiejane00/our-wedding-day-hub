0f471febf13f40adbff0216602edb3d489eb8b4d
stephiejane00 <stephiejane00@gmail.com>
2026-03-19 13:37:28 +1000

    Update firebase.js

--- docs/firebase.js
+++ docs/firebase.js
@@ -18,7 +18,6 @@ import {
   where
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDQ2gwuJoe2si8xYfhB6n9mESfSon4zRq8",
   authDomain: "ourweddingdayhub.firebaseapp.com",
@@ -28,7 +27,6 @@ const firebaseConfig = {
   appId: "1:221957124766:web:83b7ba2351c1ad656e018f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
