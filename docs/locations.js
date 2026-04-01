3c69eba403268e496626b27c772c3a9fa9e63323
stephiejane00 <stephiejane00@gmail.com>
2026-04-01 17:19:11 +1000

    Create locations.js

---
+++ docs/locations.js
@@ -0,0 +1,37 @@
+export const LOCATION_DATA = {
+  "Australia": {
+    "Queensland": ["Brisbane", "Moreton Bay", "Sunshine Coast", "Gold Coast", "Ipswich", "Redland City", "Scenic Rim", "Lockyer Valley", "Toowoomba", "Noosa", "Fraser Coast", "Whitsundays", "Cairns", "Townsville"],
+    "New South Wales": ["Sydney", "Blue Mountains", "Central Coast", "Hunter", "Illawarra", "Northern Rivers", "Mid North Coast", "Byron Bay", "Southern Highlands", "Canberra Surrounds"],
+    "Victoria": ["Melbourne", "Geelong", "Mornington Peninsula", "Yarra Valley", "Ballarat", "Bendigo", "Daylesford", "Great Ocean Road"],
+    "South Australia": ["Adelaide", "Barossa", "Fleurieu Peninsula", "Adelaide Hills", "McLaren Vale"],
+    "Western Australia": ["Perth", "South West", "Peel", "Great Southern", "Margaret River"],
+    "Tasmania": ["Hobart", "Launceston", "North West", "East Coast"],
+    "Australian Capital Territory": ["Canberra"],
+    "Northern Territory": ["Darwin", "Alice Springs"]
+  },
+
+  "New Zealand": {
+    "North Island": ["Auckland", "Wellington", "Hamilton", "Tauranga", "Napier-Hastings", "Rotorua"],
+    "South Island": ["Christchurch", "Queenstown", "Dunedin", "Nelson", "Invercargill"]
+  },
+
+  "United States": {
+    "California": ["Los Angeles", "San Diego", "San Francisco", "Sacramento", "Palm Springs", "Napa Valley"],
+    "New York": ["New York City", "Brooklyn", "Buffalo", "Albany", "Hamptons"],
+    "Texas": ["Austin", "Dallas", "Houston", "San Antonio"],
+    "Florida": ["Miami", "Orlando", "Tampa", "Palm Beach"]
+  },
+
+  "Canada": {
+    "Ontario": ["Toronto", "Ottawa", "Niagara"],
+    "British Columbia": ["Vancouver", "Victoria", "Kelowna"],
+    "Quebec": ["Montreal", "Quebec City"]
+  },
+
+  "United Kingdom": {
+    "England": ["London", "Manchester", "Birmingham", "Bath", "Cornwall"],
+    "Scotland": ["Edinburgh", "Glasgow", "Highlands", "Aberdeen"],
+    "Wales": ["Cardiff", "Swansea", "Snowdonia"],
+    "Northern Ireland": ["Belfast", "Derry"]
+  }
+};
