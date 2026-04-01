65e1ccf805c7f2b9db8590d27714c4aeb0c5110b
stephiejane00 <stephiejane00@gmail.com>
2026-04-01 14:47:49 +1000

    Update nav.js

--- docs/nav.js
+++ docs/nav.js
@@ -1,17 +1,20 @@
 import { auth, onAuthStateChanged, signOut } from "./firebase.js";
 
+const joinLink = document.getElementById("joinLink");
 const loginLink = document.getElementById("loginLink");
 const dashboardLink = document.getElementById("dashboardLink");
 const logoutLink = document.getElementById("logoutLink");
 
 onAuthStateChanged(auth, (user) => {
-  if (!loginLink || !dashboardLink || !logoutLink) return;
+  if (!loginLink || !dashboardLink || !logoutLink || !joinLink) return;
 
   if (user) {
+    joinLink.style.display = "none";
     loginLink.style.display = "none";
     dashboardLink.style.display = "inline-block";
     logoutLink.style.display = "inline-block";
   } else {
+    joinLink.style.display = "inline-block";
     loginLink.style.display = "inline-block";
     dashboardLink.style.display = "none";
     logoutLink.style.display = "none";
